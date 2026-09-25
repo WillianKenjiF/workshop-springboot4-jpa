3992e96fca6215956c882fb584c0a9b9ede319b7
Willian Kenji <willian.kenjif@hotmail.com>
2026-09-24 23:17:18 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/OrderItem.java
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -70,6 +70,10 @@ public class OrderItem implements Serializable{
 		return Objects.hash(id);
 	}
 
+	public Double getSubTotal() {
+		return quantity * price;
+	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
